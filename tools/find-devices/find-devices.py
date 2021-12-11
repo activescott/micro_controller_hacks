aa75494a9ab47dfe27e03d21725c62f2f48ce35b
Scott Willeke <scott@willeke.com>
2021-12-11 10:09:58 -0800

    feat: find-devices tool updates .env automatically

--- tools/find-devices/find-devices.py
+++ tools/find-devices/find-devices.py
@@ -2,16 +2,26 @@
 # This is a python script to run on the host machine and inspect serial port devices (general those plugged into USB)
 from serial.tools import list_ports
 from pprint import pprint
+import sys
+import os
 
 # see https://pyserial.readthedocs.io/en/latest/tools.html
 if __name__ == '__main__':
-    print("Finding MicroPython devices...")
+    print("Listing MicroPython devices...")
     ports = list_ports.comports()
     # for more detail do this:
-    #props = ['description', 'device', 'hwid', 'interface', 'location', 'manufacturer',
+    # props = ['description', 'device', 'hwid', 'interface', 'location', 'manufacturer',
     #         'name', 'pid', 'product', 'serial_number', 'vid']
     #infos = [p.__dict__ for p in ports]
-    #pprint(infos)
-    filtered = [p for p in ports if p.manufacturer=="MicroPython"]
+    # pprint(infos)
+    filtered = [p for p in ports if p.manufacturer == "MicroPython"]
     for p in filtered:
         print("  " + p.device)
+    print("Listing MicroPython devices complete. {} devices found".format(len(filtered)))
+    print("There was only one device found. Do you want to write it to `../../.env` (Y/n) ?")
+    for line in sys.stdin:
+        if line is not None and isinstance(line, str) and line.lower().startswith("y"):
+            device = filtered[0].device
+            with open("../../.env", mode="w") as f:
+                print("MP_DEVICE={}".format(device), file=f)
+        break
---
+++ tools/find-devices/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+THISDIR=$(cd $(dirname "$0"); pwd) #this script's directory
+
+pip3 install -r "$THISDIR/requirements.txt"
